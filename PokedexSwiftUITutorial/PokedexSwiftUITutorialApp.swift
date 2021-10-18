9aae89925b1aa2b1e684557dd12e23e0a15f5629
Dylan Dabrowski <dylandabrowski@gmail.com>
2021-10-18 13:00:45 -0400

    Initial Commit

---
+++ PokedexSwiftUITutorial/PokedexSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUITutorialApp.swift
+//  PokedexSwiftUITutorial
+//
+//  Created by Dylan Dabrowski on 2021-10-18.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
