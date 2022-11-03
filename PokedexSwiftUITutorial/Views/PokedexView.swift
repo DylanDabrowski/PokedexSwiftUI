ea929a4cbf2cabc69156db364bb06c8d7f2cf826
Dylan Dabrowski <dabrowsd@sheridancollege.ca>
2022-11-03 12:47:18 -0400

    list of pokemon working

---
+++ PokedexSwiftUITutorial/Views/PokedexView.swift
@@ -0,0 +1,33 @@
+//
+//  PokedexView.swift
+//  PokedexSwiftUITutorial
+//
+//  Created by Dylan Dabrowski on 2021-10-18.
+//
+
+import SwiftUI
+
+struct PokedexView: View {
+    
+    private let gridItems = [GridItem(.flexible()), GridItem(.flexible())]
+    @ObservedObject var viewModel = PokemonViewModel()
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: gridItems, spacing: 16) {
+                    ForEach(viewModel.pokemon) { pokemon in
+                        PokemonCell(pokemon: pokemon, viewModel: viewModel)
+                    }
+                }
+            }
+            .navigationTitle("Pokedex")
+        }
+    }
+}
+
+struct PokedexView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokedexView()
+    }
+}
